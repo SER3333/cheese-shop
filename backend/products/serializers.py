90dec72a75d893fa3e23a11ebd11fecfeac75077
Andrusyak <ser_333@ukr.net>
2026-01-09 14:27:31 +0200

    update

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,6 +6,45 @@ class ProductImageSerializer(serializers.ModelSerializer):
         model = ProductImage
         fields = ["image"]
 
+class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'slug',
+            'short_description',
+            'long_description',
+            'price',
+            'image',
+            'available',
+            'category',
+            'images',
+            'weight',
+        ]
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(obj.image.url)
+
+    def get_images(self, obj):
+        request = self.context.get("request")
+        return [
+            {"image": request.build_absolute_uri(img.image.url)}
+            for img in obj.images.all()
+        ]
+'''
+from rest_framework import serializers
+from .models import Product, ProductImage
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["image"]
+
 class ProductListSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
@@ -54,3 +93,5 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         if request:
             return request.build_absolute_uri(obj.image.url)
         return obj.image.url
+
+'''
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,25 @@
 from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.filter(available=True).order_by('-created_at')
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {"request": self.request}
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.filter(available=True)
+    serializer_class = ProductSerializer
+    lookup_field = "slug"  # <- ось це ключове
+
+    def get_serializer_context(self):
+        return {"request": self.request}
+
+'''
+from rest_framework import generics
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from .models import Product
@@ -31,3 +52,4 @@ class ProductDetailView(generics.RetrieveAPIView):
 
     def get_serializer_context(self):
         return {"request": self.request}
+'''
