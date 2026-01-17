c05fe82f3fac6364d9e5e99a207c15d2ff9d1bce
Andrusyak <ser_333@ukr.net>
2026-01-17 19:08:45 +0200

    update

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,55 +1,36 @@
 from rest_framework import serializers
-from .models import Product, ProductImage, ProductReview
+from .models import Product, ProductReview
+
 
 class ProductReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductReview
         fields = ["id", "name", "rating", "comment", "created_at"]
 
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ["image"]
 
+# 🔹 СПИСОК ТОВАРІВ
 class ProductListSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-    images = serializers.SerializerMethodField()
-    average_rating = serializers.FloatField()
-
-
+    image = serializers.ImageField(read_only=True)
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = [
-            'id',
-            'name',
-            'slug',
-            'short_description',
-            'long_description',
-            'price',
-            'image',
-            'available',
-            'category',
-            'images',
-            'weight',
+            "id",
+            "name",
+            "slug",
+            "price",
+            "weight",
+            "category",
+            "image",
             "average_rating",
-            "reviews",
-        ]
-
-    def get_image(self, obj):
-        request = self.context.get("request")
-        return request.build_absolute_uri(obj.image.url)
-
-    def get_images(self, obj):
-        request = self.context.get("request")
-        return [
-            {"image": request.build_absolute_uri(img.image.url)}
-            for img in obj.images.all()
         ]
 
     def get_average_rating(self, obj):
         return obj.average_rating()
 
+
+# 🔹 ДЕТАЛЬНА СТОРІНКА
 class ProductDetailSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(read_only=True)
     images = serializers.SerializerMethodField()
