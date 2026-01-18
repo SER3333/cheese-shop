201fdd455ed6218a2e8aac1a526ca50d9100455e
Andrusyak <ser_333@ukr.net>
2026-01-18 12:44:35 +0200

    update

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,6 @@ from .views import ProductListView, ProductDetailView, ProductReviewCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
-    path('<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),  # SEO-friendly slug
     path("reviews/create/", ProductReviewCreateView.as_view(), name="review-create"),
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),  # SEO-friendly slug
 ]
